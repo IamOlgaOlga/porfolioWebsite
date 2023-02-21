8e051ad7d6d70ce5fe3eb7a999bcfbc709e7e7fc
iamolgaolga <iamolgaostrikova@gmail.com>
2023-02-21 19:44:07 +0400

    add small fix

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -12,12 +12,12 @@ function Intro() {
                 </div>
                 <div class="place-self-start">
                     <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Olga Ostrikova</h1>
-                    <p className="text-base md:text-xl mb-3 font-medium">Software Developer, <div class="pin"></div> Dubai, UAE</p>
-                    <p className="text-sm md:text-sm mb-6 font-bold">Software Developer with 7+ years of experience in international IT companies. Here is my{' '}
+                    <p className="text-base md:text-xl mb-3 font-medium">Software Engineer, <div class="pin"></div> Dubai, UAE</p>
+                    <p className="text-sm md:text-sm mb-6 font-bold">Software Engineer with 7+ years of experience in international IT companies. Here is my{' '}
             <a href="https://www.linkedin.com/in/iamolgaostrikova" 
             target="_blank" 
             className="text-blue-500 dark:text-fuchsia-300 hover:underline underline-offset-2 decoration-2 decoration-fuchsia-300 dark:decoration-blue-300" rel="noreferrer noopener">
-                LinkedIn profile</a>{' '}
+                LinkedIn profile</a>.
                 </p>
                 </div>
             </div>
--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,20 +1,20 @@
 export default [
     {
         year: '2020',
-        title: 'Github activity',
+        title: 'Senior Software Engineer at Cardpay(Unlimint) company',
         duration: '1 year and 7 months',
         details: 'I worked on one of the core payment teams. I developed a reverse-proxy service responsible for mobile clients applications\' integrity and security vulnerability checks and a part Mobile API which unlocked integrations with Android/iOS applications. This project unblocked signing of multiple contracts for the company. I led a team of 6 developers/test engineers and 2 analysts on several cross-functional projects which added support of different payment platforms. This included team and department roadmap planning, team staffing, mentorship of junior and middle engineers, cross-team collaboration with Security, Legal, Product and Design teams. Implementing these projects opened new streams of customers from LATAM and EMEA regions and increased department ARR.'
 
     },
     {
         year: '2017',
-        title: 'Senior Software engineer at DELL EMC company',
+        title: 'Senior Software Engineer at DELL EMC company',
         duration: '3 years and 2 months',
         details: 'I worked on a storage array sizing tool. My work included several performance and overall stability improvements along with redesign and refactoring of the product. Apart from that, I\'ve maintained a legacy version of the product being in use by existing customers, which included interaction and troubleshooting with customers. I also took part in DellEMC student program as a mentor.'
     },
     {
         year: '2015',
-        title: 'Software engineer at EMC company',
+        title: 'Software Engineer at EMC company',
         duration: '2 years and 2 months',
         details: 'I worked on a storage array sizing tool. I improved and updated the configuration and sizing algorithm to cover new features of updated storage arrays. Apart from that, I developed a tool used for reports generating. These reports describe condition and utilisation of elements and devices of a storage array. This reduced the time of report generation (which was formerly done manually) from 2 weeks to 5 minutes. During this project I gained deep understanding about storage arrays internals and passed EMC SPEED Performance Guru exam.'
 
